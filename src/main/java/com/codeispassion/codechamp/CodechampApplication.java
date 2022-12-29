e9c93f6b753ed866709cf4c3a9cdd12843a1044d
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 02:46:56 +0530

    initial commit

---
+++ src/main/java/com/codeispassion/codechamp/CodechampApplication.java
@@ -0,0 +1,13 @@
+package com.codeispassion.codechamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodechampApplication.class, args);
+	}
+
+}
