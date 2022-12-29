33cf00ef2159b37e5f43d2952140f8f82577e514
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 02:48:55 +0530

    Add test

---
+++ src/test/java/com/codeispassion/codechamp/CodechampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeispassion.codechamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
