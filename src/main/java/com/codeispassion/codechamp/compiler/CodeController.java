dcba01224f20c9e6f0bb799310354553914cac98
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 07:07:29 +0530

    Remove CORS error
    Add annotation @CrossOrigin("*")
    make sure to add header into the client request body

--- src/main/java/com/codeispassion/codechamp/compiler/CodeController.java
+++ src/main/java/com/codeispassion/codechamp/compiler/CodeController.java
@@ -3,14 +3,18 @@ package com.codeispassion.codechamp.compiler;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codeispassion.codechamp.compiler.coderesponse.CodeResponse;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class CodeController {
 
     @Autowired
@@ -21,13 +25,18 @@ public class CodeController {
         return "Test Date: " + new Date();
     }
 
-    @PostMapping("/compile")
-    public String compileCode(@RequestBody Code code) {
-        return code.getCompileResult();
+    @GetMapping("/response")
+    public CodeResponse getResponse() {
+        return new CodeResponse();
     }
 
-    @GetMapping
+    @PostMapping("/compile")
+    public CodeResponse compileCode(@RequestBody Code code) {
+        return codeService.compileCode(code);
+    }
+
+    @GetMapping("/code")
     public Code getDefaultCode() {
-        return new Code("test1", LanguageType.PYTHON, "print('hello')", "hello python result");
+        return new Code("test1", LanguageType.PYTHON, "print('hello')");
     }
 }
