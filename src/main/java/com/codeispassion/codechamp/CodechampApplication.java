e0bbea555e99023caca2c0c65fb15fa66c04c275
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-04 23:06:20 +0530

    Add @EnableMongoRepositories annotation in main class

--- src/main/java/com/codeispassion/codechamp/CodechampApplication.java
+++ src/main/java/com/codeispassion/codechamp/CodechampApplication.java
@@ -2,12 +2,15 @@ package com.codeispassion.codechamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class CodechampApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodechampApplication.class, args);
+
 	}
 
 }
