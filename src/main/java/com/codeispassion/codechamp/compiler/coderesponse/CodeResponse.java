3e671480f6af9d5f3773380149ea997bd4dd538f
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-06 12:13:25 +0530

    Convert code response to Mongo Document model

--- src/main/java/com/codeispassion/codechamp/compiler/coderesponse/CodeResponse.java
+++ src/main/java/com/codeispassion/codechamp/compiler/coderesponse/CodeResponse.java
@@ -1,9 +1,9 @@
 package com.codeispassion.codechamp.compiler.coderesponse;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+@Document("coderesponses")
 public class CodeResponse {
     @Id
     private String codeResponseId;
