66aa31d69b76fa29e0c4ef046e91927ed4c4cdb1
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-07 00:22:20 +0530

    convert JpaRepository to MongoRepostiory

--- src/main/java/com/codeispassion/codechamp/compiler/CodeRepository.java
+++ src/main/java/com/codeispassion/codechamp/compiler/CodeRepository.java
@@ -1,7 +1,8 @@
 package com.codeispassion.codechamp.compiler;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CodeRepository extends JpaRepository<Code, String> {
+public interface CodeRepository extends MongoRepository<Code, String> {
 
 }
