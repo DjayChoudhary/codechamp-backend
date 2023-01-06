9a72d2b6519bc5c2d14827b0943a18fc593f86fe
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-07 00:24:28 +0530

    add Autowired CodeResponseRepository instance

--- src/main/java/com/codeispassion/codechamp/compiler/CodeService.java
+++ src/main/java/com/codeispassion/codechamp/compiler/CodeService.java
@@ -1,9 +1,12 @@
 package com.codeispassion.codechamp.compiler;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codeispassion.codechamp.compiler.coderesponse.CodeResponse;
+import com.codeispassion.codechamp.compiler.coderesponse.CodeResponseRepository;
 import com.codeispassion.codechamp.compiler.coderesponse.CodeResponseStatus;
 
 @Service
@@ -11,6 +14,9 @@ public class CodeService {
 
     @Autowired
     CodeRepository codeRepository;
+
+    @Autowired
+    CodeResponseRepository codeResponseRepository;
 
     public CodeResponse compileCode(Code code) {
         // codeRepository.findById(null).orElse(null);
@@ -37,7 +43,11 @@ public class CodeService {
                 break;
         }
 
-        return new CodeResponse(code.getId(), status, response);
+        codeRepository.save(code);
+
+        CodeResponse cr = new CodeResponse(code.getId(), status, response);
+        codeResponseRepository.save(cr);
+        return cr;
     }
 
     private String compilePython(String codeBody) {
@@ -54,6 +64,10 @@ public class CodeService {
 
     private String compileJs(String codeBody) {
         return "default JavaScript response";
+    }
+
+    public List<CodeResponse> getAllCodeResponses() {
+        return codeResponseRepository.findAll();
     }
 
 }
