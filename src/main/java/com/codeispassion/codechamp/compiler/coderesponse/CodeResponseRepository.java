94aa2b272c4e226ce6332af705a29ae2718150cd
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-07 00:25:13 +0530

    Create Code Response Repository

---
+++ src/main/java/com/codeispassion/codechamp/compiler/coderesponse/CodeResponseRepository.java
@@ -0,0 +1,7 @@
+package com.codeispassion.codechamp.compiler.coderesponse;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CodeResponseRepository extends MongoRepository<CodeResponse, String> {
+
+}
